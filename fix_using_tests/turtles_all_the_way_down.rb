775ca20e1b96a6ccac85a4aa2e0d573b3a92be7c
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-29 18:27:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -11,7 +11,7 @@ end
 
 def turtle_traits(turtles)
   turtles.map do |turtle|
-    binding.pry
+    # binding.pry
       if turtle[:traits].is_a?(Array)
           turtle[:traits].each do |trait|
           trait
